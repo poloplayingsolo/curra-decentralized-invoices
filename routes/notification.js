4b1c1629baa46069258665d5ec7ae4a875c7426e
Dmytro Pintak <dmytro.pintak@gmail.com>
2022-11-20 00:15:14 +0200

    fix(notifications): added end + more fields from body

--- routes/notification.js
+++ routes/notification.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 /* POST curra connector notification */
 router.post("/notifications", async function (req, res, next) {
+  if (req.body.value === "0" || !req.body.toAddress.owned) {
+    res.status(200).send();
+    return;
+  }
   console.log("-----NOTIFICATION------");
-  console.log("Value:", req.body.value);
-  console.log("From address:", req.body.fromAddress.value);
-  console.log("Tx hash:", req.body.txHash);
-  console.log("Token:", req.body.token);
+  console.log(req.body);
   // your logic goes above. for example: increse user balance
+  res.status(200).send();
 });
 
 module.exports = router;
