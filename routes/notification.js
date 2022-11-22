cab938394de01c55c47b9beaa59564f47ec35c19
Dmytro Pintak <dmytro.pintak@gmail.com>
2022-11-22 20:31:02 +0200

    feat(global): node update

--- routes/notification.js
+++ routes/notification.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* POST curra connector notification */
 router.post("/notifications", async function (req, res, next) {
-  if (req.body.value === "0" || !req.body.toAddress.owned) {
+  const transfer = req.body;
+  if (
+    !req.curra.isIncomingTransfer(transfer) ||
+    !req.curra.isNonZeroTransfer(transfer)
+  ) {
     res.status(200).send();
     return;
   }
   console.log("-----NOTIFICATION------");
-  console.log(req.body);
+  console.log(transfer);
   // your logic goes above. for example: increse user balance
   res.status(200).send();
 });
